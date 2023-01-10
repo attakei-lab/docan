92023d057b60b418b8a5837649f84a899a4844c6
Kazuya Takei <myself@attakei.net>
2023-01-10 21:38:49 +0900

    doc(ja): Init ja documentation

---
+++ doc-ja/conf.py
@@ -0,0 +1,17 @@
+"""Configuration for docmentation."""
+
+# -- Project information
+project = "DOCAN(日本語)"
+copyright = "2023, Kazuya Takei"
+author = "Kazuya Takei"
+release = "0.0.1"
+
+# -- General configuration
+extensions = []
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+language = "ja"
+
+# -- Options for HTML output
+html_theme = "alabaster"
+html_static_path = ["_static"]
